e6c209e79d63fd1f471efd35a2e456452cb92805
cacheflowe <cacheflowe@cacheflowe.com>
2015-08-13 18:22:15 -0600

    Cleaned up demo and added a generative shader plane

--- javascripts/shader-day-demo.js
+++ javascripts/shader-day-demo.js
@@ -1,20 +1,9 @@
-var uniforms = {
-  ambientLightColor:{ type:"c" , value:new THREE.Color( 0xffffff) },
-  diffuseLightColor:{ type:"c" , value:new THREE.Color( 0xffffff ) },
-  specularLightColor:{ type:"c" , value:new THREE.Color( 0xffffff ) },
+// global THREE.js/scene elements
+var camera, renderer, scene, controls;
+var w = window.innerWidth;
+var h = window.innerHeight;
 
-  ambientMaterialColor:{ type:"c" , value:new THREE.Color( 0x000fee ) },
-  diffuseMaterialColor:{ type:"c" , value:new THREE.Color( 0xc0aa99 ) },
-  specularMaterialColor:{ type:"c" , value:new THREE.Color( 0xff0000 ) },
-
-  shininess:{ type:"f" , value:80 },
-  lightPosition:{ type:"v3" , value: new THREE.Vector3( 300 , 300 , 300 ) }
-};
-
-var camera, renderer, scene , controls;
-var vs, fs;
-var geometry, material , light;
-
+// load shaders and fire up THREE scene once we've completed the async load
 var shaders = new ShaderLoader('./shaders', './shaderChunks');
 shaders.shaderSetLoaded = function(){
   init();
@@ -22,47 +11,97 @@ shaders.shaderSetLoaded = function(){
 }
 shaders.load('vs-ads', 'ads', 'vertex');
 shaders.load('fs-ads', 'ads', 'fragment');
+shaders.load('vs-default', 'default', 'vertex');
+shaders.load('fs-dots', 'dots', 'fragment');
 
 function init(){
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-
+  // build rotatable camera & scene
   camera = new THREE.PerspectiveCamera( 65 , w/h , 1 , 2000 );
   camera.position.z = 1000;
-
   controls = new THREE.TrackballControls( camera );
   scene = new THREE.Scene();
 
-  light = new THREE.PointLight( 0xff00ff , 3 , 1000 );
+  buildLight();
+  buildSphere();
+  buildShaderPlane();
+
+  // fire up the webGL <canvas> renderer and attach it to the DOM
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+}
+
+function buildLight() {
+  // add a light
+  var light = new THREE.PointLight( 0xff00ff , 3 , 1000 );
   light.position.set( 300 , 300 ,300 );
   scene.add( light );
 
+  // show where the light is with a dummy object
   lightMarker = new THREE.Mesh(
     new THREE.IcosahedronGeometry( 10 , 1 ),
     new THREE.MeshBasicMaterial({ color: light.color })
   );
   light.add( lightMarker );
+}
 
-  geometry = new THREE.SphereGeometry( 200, 20, 20 );
+function buildSphere() {
+  // create the main sphere for the scene
+  var geometry = new THREE.SphereGeometry( 200, 20, 20 );
   geometry.computeVertexNormals();
 
-  material = new THREE.ShaderMaterial({
+  var uniforms = {
+    ambientLightColor:{ type:"c" , value:new THREE.Color( 0xffffff) },
+    diffuseLightColor:{ type:"c" , value:new THREE.Color( 0xffffff ) },
+    specularLightColor:{ type:"c" , value:new THREE.Color( 0xffffff ) },
+
+    ambientMaterialColor:{ type:"c" , value:new THREE.Color( 0x000fee ) },
+    diffuseMaterialColor:{ type:"c" , value:new THREE.Color( 0xc0aa99 ) },
+    specularMaterialColor:{ type:"c" , value:new THREE.Color( 0xff0000 ) },
+
+    shininess:{ type:"f" , value:80 },
+    lightPosition:{ type:"v3" , value: new THREE.Vector3( 300 , 300 , 300 ) }
+  };
+
+  var material = new THREE.ShaderMaterial({
     uniforms:       uniforms,
     vertexShader:   shaders.vertexShaders.ads,
     fragmentShader: shaders.fragmentShaders.ads,
   });
 
-  mesh = new THREE.Mesh( geometry, material );
+  var mesh = new THREE.Mesh( geometry, material );
   scene.add( mesh );
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-  document.body.appendChild( renderer.domElement );
 }
 
+function buildShaderPlane() {
+  // Create a plane with a generative shader texture.
+  // The vUv varying is super important in the fragment shader to display the proper plane perspective here.
+  // If the vUv varying is not used, the texture will be *unmasked* by the plane, rather than appear attached to the plane...
+  // see: http://mrdoob.github.io/three.js/examples/webgl_shader2.html
+  // or http://mrdoob.github.io/three.js/examples/webgl_shader.html for a flat full-screen texture shader display
+  // docs: http://threejs.org/docs/#Reference/Materials/ShaderMaterial
+  window.planeTextureUniforms = {
+    time:{ type:"f" , value:0 }
+  };
+
+  var planeMat = new THREE.ShaderMaterial({
+    uniforms:       planeTextureUniforms,
+    vertexShader:   shaders.vertexShaders.default,
+    fragmentShader: shaders.fragmentShaders.dots,
+    side: THREE.DoubleSide
+  });
+  var planeGeom = new THREE.PlaneGeometry(w/2, h/2);
+  var mesh = new THREE.Mesh(planeGeom, planeMat);
+  scene.add( mesh );
+}
+
+// render loop
 function animate(){
-  requestAnimationFrame( animate );
+  // update time uniform on generative texture shader
+  planeTextureUniforms.time.value = Math.sin(Date.now()/1000);
+  // update camera
   controls.update();
+  // render and keep animating
+  requestAnimationFrame( animate );
   renderer.render( scene , camera );
 }
